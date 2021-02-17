fb3549322e8563c7ab3aeb11a2cfb66d3ec52a4e
jirlinge <julienirlinger@gmail.com>
2021-02-17 14:53:57 +0100

    version corrigé

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            while (n < 10) {
+                Console.WriteLine("Bonjour je ne suis qu'un message");
+                n++;
+            }
+        }
+    }
+}
